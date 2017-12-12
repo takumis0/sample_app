afb33e32b066aa6428243a6d9fb720680f12c099
Takumi Sekiguchi <takumis2013@yahoo.co.jp>
2017-12-12 00:31:19 +0000

    finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ SampleApp::Application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
-  get "users/show"
-  devise_for :users
   resources :users, only: [:show]
   root "static_pages#home"
   match '/help',    to: 'static_pages#help',    via: 'get'
