319f5698c1757c62f9dcf9b9a36bdcff44e64c15
Takumi Sekiguchi <takumis2013@yahoo.co.jp>
2017-12-12 00:11:16 +0000

    finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SampleApp::Application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   get "users/show"
   devise_for :users
   resources :users, only: [:show]
