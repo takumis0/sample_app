b63d5fc3671701957d7517f5fb5f5000ffc3cf32
Takumi Sekiguchi <takumis2013@yahoo.co.jp>
2018-01-03 01:40:08 +0000

    Make a Micropost model

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
   # 該当ユーザーのメールアドレスに対応するGravatarの画像URLを返す
-  def gravatar_for(user, options = {size:50})
+  def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
